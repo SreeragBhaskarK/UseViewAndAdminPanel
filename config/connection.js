ea860cdbf6998dd17eed3e2ead892b79dafaa7ef
SREERAG BHASKAR K <46321648+SreeragBhaskarK@users.noreply.github.com>
2023-02-03 02:56:03 +0530

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const state={
 }
 
 module.exports.connect=function(done){
-    const url = "mongodb://127.0.0.1:27017"
+    const url = "mongodb+srv://sreerag:<password>@cluster0.nbvlgji.mongodb.net/?retryWrites=true&w=majority"
     const dbname= 'shopping'
 
     mongoClient.connect(url,(err,data)=>{
